477840be3ea41776713d15cb6e7b688e83256881
Andy Kuszyk <andykuszyk@live.co.uk>
2017-11-04 10:28:44 +0000

    Updated to version 0.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='simpcass',
     packages=['simpcass'],
-    version='0.1.2',
+    version='0.1.3',
     description='A simple client for Apache Cassandra',
     author='Andy Kuszyk',
     author_email='pairofsocks@hotmail.co.uk',
